41c9f0d9da380727fb02f185df330503c6dbe5f3
Shubham Chauhan <shubham.cse@outlook.com>
2015-08-07 13:13:44 +0530

    KML Layer
    Added library import

--- app/src/main/java/com/randomsegment/apn/ismshuttle/MapsActivity.java
+++ app/src/main/java/com/randomsegment/apn/ismshuttle/MapsActivity.java
@@ -18,6 +18,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.kml.KmlLayer;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
