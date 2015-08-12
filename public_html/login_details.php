8e39c160bc84ab8b302d735b031d125fe683304b
Prateek Nischal <prateeknischal25@gmail.com>
2015-08-13 02:54:25 +0530

    removed login file

--- public_html/login_details.php
+++
@@ -1,9 +0,0 @@
-<?php 
-class Login_details{
-	static $SERVER_NAME = "mysql10.000webhost.com";
-	static $USERNAME = "a2189285_pikaynu";
-	static $PASSWORD = "pikaynu.123";
-	static $DB_NAME = "a2189285_log";
-}
-
-?>
