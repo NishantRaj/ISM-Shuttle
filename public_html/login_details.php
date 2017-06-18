a58f956c17e24b99261ed5a7928d7252ef982fa7
prateeknischal <prateeknischal25@gmail.com>
2017-06-18 23:04:53 +0530

    Update login_details.php

--- public_html/login_details.php
+++ public_html/login_details.php
@@ -2,7 +2,7 @@
 class Login_details{
 	static $SERVER_NAME = "mysql10.000webhost.com";
 	static $USERNAME = "a2189285_pikaynu";
-	static $PASSWORD = "pikaynu.123";
+	static $PASSWORD = "xxxxxxx";
 	static $DB_NAME = "a2189285_log";
 }
 
